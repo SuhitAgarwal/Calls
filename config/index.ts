251f9cb5d83bed41fd60d4ff2ae7666f8df55a2f
Suhit Agarwal <suhitaga@usc.edu>
2021-02-21 20:58:22 +0530

    dumb bug!

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 export const isDevURL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://streamwithfriends-online.vercel.app/"
+    : "https://streamwithfriends-online.vercel.app"
